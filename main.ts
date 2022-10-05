a70fed2fd3189cf138fbfdcacc4a3efafcd1de6e
pc2d34 <115052121+pc2d34@users.noreply.github.com>
2022-10-05 09:44:17 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
+let number = 0
+let count = 0
+input.onButtonPressed(Button.A, function () {
+    number = 1
+    for (let index = 0; index < 6; index++) {
+        basic.showNumber(number)
+        basic.pause(1000)
+        number += 1
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    count = 9
+    for (let index = 0; index < 10; index++) {
+        basic.showNumber(count)
+        basic.pause(1000)
+        count += -1
+    }
+})
 basic.forever(function () {
 	
 })
